2267977ec81212b0c48d7f48cd06a57ca5f164d1
陈舒莹 <10703457+zzangyou@user.noreply.gitee.com>
2022-11-02 21:24:27 +0800

    icon

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import { router } from './router';
 import './style.css';
 import 'reset-css';
 import App from './App.vue';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // import './styles/element/index.scss'
 const pinia = createPinia();
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  createApp(App).component(key, component)
+}
 createApp(App).use(pinia).use(router).mount('#app');
